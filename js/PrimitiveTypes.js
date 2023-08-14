0915f2f4f94f648e2910a8d463f0d07802faeb39
Johnny <mrtnz.jn@gmail.com>
2023-08-14 11:42:44 -0500

    Completed Primitive Types exercises for JavaScript I module

---
+++ js/PrimitiveTypes.js
@@ -0,0 +1,56 @@
+// Exercises
+// Identify the data type of the following values:
+//
+//
+//  99.9
+// THIS IS A NUMBER
+
+
+// "false"
+// THIS IS A STRING
+
+
+// false
+//THIS IS A BOOLEAN
+
+
+// '0'
+// THIS IS A STRING
+
+
+// 0
+// THIS IS A NUMBER
+
+
+// true
+// THIS IS A BOOLEAN
+
+
+// 'true'
+// THIS IS A STRING
+
+
+// What data type would best represent:
+//
+//     A term or phrase typed into a search box?
+//      STRING
+
+
+//     If a user is logged in?
+//      BOOLEAN
+
+
+//     A discount amount to apply to a user's shopping cart?
+//      NUMBER
+
+
+// Whether or not a coupon code is valid?
+//      BOOLEAN
+
+
+//     An email address typed into a registration form?
+//      STRING
+
+
+//     The price of a product?
+//      NUMBER
