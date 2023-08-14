05fb2084ef25444c1d5707a68d5ae076ed9e60f9
Johnny <mrtnz.jn@gmail.com>
2023-08-14 12:20:01 -0500

    Completed Operators exercises for JavaScript I module

---
+++ js/Operators.js
@@ -0,0 +1,75 @@
+// Exercises
+// For each of the following code blocks, read the expression and predict what the result of evaluating it would be, then execute the expression in the Chrome console.
+//
+//
+// '1' + 2;
+// 12
+console.log('1' + 2);
+
+// typeof '1' + 2
+// STRING
+console.log(typeof ("1" + 2));
+
+// 6 % 4
+// 2
+console.log(6 % 4);
+
+// '3 + 4 is ' + 3 + 4
+// // how could you make this produce the correct output?
+console.log("3 + 4 is " + (3 + 4));
+
+// 0 < 0
+// FALSE
+console.log(0 < 0);
+
+// 'false' == false
+// FALSE
+console.log('false' == false);
+
+// true == 'true'
+// FALSE
+console.log(true == 'true');
+
+// 5 >= -5
+// TRUE
+console.log(5 >= -5);
+
+// !false || false
+// TRUE
+console.log(!false || false);
+
+// true || "42"
+// TRUE
+console.log(true || "42");
+
+// !true && !false
+// FALSE
+console.log(!true && !false);
+
+// 6 % 5
+// 1
+console.log(6 % 5);
+
+// 5 < 4 && 1 === 1
+// FALSE
+console.log(5 < 4 && 1 === 1);
+
+// typeof 'codeup' === 'string'
+// BOOLEAN
+console.log(typeof ('codeup' === 'string'));
+
+// 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+// FALSE
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+
+// 4 >= 0 && 1 !== '1'
+// TRUE
+console.log(4 >= 0 && 1 !== '1');
+
+// 6 % 3 === 0
+// TRUE
+console.log(6 % 3 === 0);
+
+// 5 % 2 !== 0
+// TRUE
+console.log(5 % 2 !== 0);
