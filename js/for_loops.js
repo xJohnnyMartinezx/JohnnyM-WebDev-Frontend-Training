cfae3d129df27936ce85fbc889f2fc3208a0babe
Johnny <mrtnz.jn@gmail.com>
2023-08-21 14:02:43 -0500

    completed exercises 1 and 2 for JS-I - Loops lesson.

---
+++ js/for_loops.js
@@ -0,0 +1,57 @@
+"use strict";
+(function(){
+
+
+    // ******* EXERCISE 1 *********
+    // Create a file named for_loops.js inside the js directory and link it to your loops.html file.
+
+
+        // ******* EXERCISE 2 *********
+    function showMultiplicationTable(num){
+        // ****** FOR LOOP ******
+        // for (let i = 1; i <= 10 ; i++) {
+        //     console.log(`${num} x ${i} = ${num * i}`);
+        // }
+
+        // ****** WHILE LOOP ******
+        let i = 1;
+        while (i <= 10){
+            console.log(`${num} x ${i} = ${num * i}`);
+            i++;
+        }
+    }
+    showMultiplicationTable(7);
+
+
+
+    // ******* EXERCISE 3 *********
+
+
+
+    // ******* EXERCISE 4 *********
+
+
+    // ******* EXERCISE 5 *********
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
