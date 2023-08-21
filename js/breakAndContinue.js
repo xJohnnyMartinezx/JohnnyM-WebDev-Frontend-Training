c5ffd5bbb104ceb40c62230ebf5bfc52b75625d6
Johnny <mrtnz.jn@gmail.com>
2023-08-21 16:50:58 -0500

    completed exercises 1 - 5 For Loops and #2 Break and Continue for JS-I - Loops lesson.

---
+++ js/breakAndContinue.js
@@ -0,0 +1,48 @@
+"use strict";
+(function (){
+
+
+    // ******* EXERCISE 1 *********
+    // Create a file named break_and_continue.js in the js directory.
+
+
+    // ******* EXERCISE 2 *********
+    // Prompt the user for an odd number between 1 and 50. Use a loop and a break
+    // statement to continue prompting the user if they enter invalid input.
+
+    function userOddOneThroughFifty() {
+        for (let i = 1; i <= 50; i++) {
+            let userInput = prompt("Enter an odd number between 1 and 50.");
+            if (!isNaN(parseInt(userInput)) && userInput % 2 !== 0 && userInput >= 1 && userInput <= 50) {
+                break;
+            }
+        }
+    }
+    userOddOneThroughFifty();
+
+    console.log("**************************");
+
+    // ******* EXERCISE 3 *********
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
--- js/for_loops.js
+++ js/for_loops.js
@@ -6,7 +6,10 @@
     // Create a file named for_loops.js inside the js directory and link it to your loops.html file.
 
 
-        // ******* EXERCISE 2 *********
+    // ******* EXERCISE 2 *********
+    // Create a function named showMultiplicationTable that accepts a number and console.logs
+    // the multiplication table for that number (just multiply by the numbers 1 through 10)
+
     function showMultiplicationTable(num){
         // ****** FOR LOOP ******
         // for (let i = 1; i <= 10 ; i++) {
@@ -22,36 +25,91 @@
     }
     showMultiplicationTable(7);
 
-
+    console.log("***************************");
 
     // ******* EXERCISE 3 *********
+    // Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and
+    // output to the console whether each number is odd or even.
 
 
+
+
+        function randomOddEven(){
+            for (let i = 1; i <= 10; i++){
+                let randomNum = Math.floor(Math.random() * (201 - 20) + 1);
+                if (randomNum %2 === 0){
+                    console.log(`${randomNum} is even.`);
+                } else {
+                    console.log(`${randomNum} is odd.`);
+                }
+            }
+        }
+
+        randomOddEven();
+
+    console.log("***************************");
 
     // ******* EXERCISE 4 *********
+    // Create a for loop that uses console.log to create the output shown below.
 
+    // 1
+    // 22
+    // 333
+    // 4444
+    // 55555
+    // 666666
+    // 7777777
+    // 88888888
+    // 999999999
+
+    function pyramid(){
+        // let order = "";
+        for (let i = 1; i < 10; i++) {
+            // SETTING AN EMPTY STRING VARIABLE;
+            let order = "";
+            // SETS THE FIRST DIGIT IN EACH ITERATION;
+            order += i;
+                // NESTED FOR LOOP WILL CONTINUE TO ITERATE AS LONG AS j IS LESS THAN i;
+                for (let j = 1; j < i; j++) {
+                    order += i;
+                }
+            console.log(order);
+        }
+    }
+
+    pyramid();
+
+
+    console.log("***************************");
 
     // ******* EXERCISE 5 *********
+    // Create a for loop that uses console.log to create the output shown below.
+    // 100
+    // 95
+    // 90
+    // 85
+    // 80
+    // 75
+    // 70
+    // 65
+    // 60
+    // 55
+    // 50
+    // 45
+    // 40
+    // 35
+    // 30
+    // 25
+    // 20
+    // 15
+    // 10
+    // 5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    function countFives(){
+        for (let i = 100; i >= 5; i-=5) {
+            console.log(i);
+        }
+    }
+    countFives();
 
 })();
